26b8d3c51b75ab9295d92ea8fc087786f4cfdba6
MartyCodeClan <zabbalab@hotmail.com>
2017-10-03 21:32:52 +0100

    homework commit

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+Show.delete_all
+Show.create({title: "AK47", series: 47, description: "Guns, guns, guns", image: "AK", programmeID: "b013pqnm"})
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
